81c4e37e531a62ec2533d678dc221db592f24402
coque <coquelet.samuel@gmail.com>
2021-11-15 17:40:47 +0100

    terminus

---
+++ app.js
@@ -0,0 +1,16 @@
+let paragraph = document.getElementsByClassName("paragraphe");
+
+for (let i = 0; i < paragraph.length; i++){
+
+    if ((i + 1) % 2 === 0){
+        continue;
+    }
+
+    else if (i === paragraph.length){
+        break;
+    }
+
+    else {
+        paragraph[i].innerHTML = i + 1;
+    }
+}
